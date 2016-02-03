10a6a770fc80f37ceb40411d374282a716b07fbc
yusureabc <czhyusure@163.com>
2016-02-03 12:06:04 +0800

    1、绑定到 Home 模块

--- web/index.php
+++ web/index.php
@@ -17,6 +17,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
+// 绑定到 Home 模块
+define( 'BIND_MODULE', 'Home' );
+
 // 定义应用目录
 define('APP_PATH','./Application/');
 
